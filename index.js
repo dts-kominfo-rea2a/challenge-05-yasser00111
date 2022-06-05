004555027de4758f546e2c321f92b6dbe9e596bb
yasser00111m <yassertkj@gmail.com>
2022-06-05 22:53:24 +0800

    callback chalenge done

--- index.js
+++ index.js
@@ -6,25 +6,43 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (arr, sort) => {
+    sortdata = sort(arr);
+
+    let data = []
+    for (let index = 0; index < sortdata.length; index++) {
+        let loop = ''
+        loop += index + 1 + '.' + sortdata[index]
+            //tambahkan array
+        data.push(loop)
+    }
+    return data;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+
+const sortAscending = arr => {
+    let data = arr.sort();
+    return data;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (arr) => {
+    let data = arr.sort().reverse()
+    return data;
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
-  console.log(sorter?.(names, sortAscending)?.join("\n"));
-  console.log(sorter?.(names, sortDescending)?.join("\n"));
+    console.log(sorter ? .(names, sortAscending) ? .join("\n"));
+    console.log(sorter ? .(names, sortDescending) ? .join("\n"));
 })();
 
 module.exports = {
-  sorter,
-  sortAscending,
-  sortDescending,
-  names,
+    sorter,
+    sortAscending,
+    sortDescending,
+    names,
 };
